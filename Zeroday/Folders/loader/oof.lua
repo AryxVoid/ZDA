783c97465e1fe2b72edb81b91213301597d2583a
Auth Zero <f2v74yt@gmail.com>
2022-02-07 19:24:56 -0600

    Update oof.lua

--- Zeroday/Folders/loader/oof.lua
+++ Zeroday/Folders/loader/oof.lua
@@ -2,10 +2,12 @@ local util = {}
 local zday = {}
 
 zday.client = {}
+zday.client.cmds = {}
 
 --init player
 ws = game.Workspace
 players = game:GetService'Players'
+zdayuser = players.LocalPlayer
 myname = players.LocalPlayer.Name
 myid = players.LocalPlayer.UserId
 
@@ -17,7 +19,10 @@ util.info = {
    version = "Z_0.0.1",
    patched = "Z_0.0.0",
    myuser = myname,
-   myuserid = myid
+   myuserid = myid,
+   user = zdayuser,
+   adminname = "Zero Day",
+   admintype = "Global"
 }
 
 
@@ -35,9 +40,41 @@ util.precheck = function()
     return true
 end
 
-zday.client.init = function(game)
+zday.client.init = function()
    if util.precheck() then
-       print('Test')
+       print('Zero Day Loaded!')
+       wait()
+
+
+zday.client.cmds.test = function()
+        print("Hello World,"..util.info.adminname.." Working...")
+end
+
+
+
+
+
+
+
+
+
+zdayuser.Chatted:Connect(function(msg,recipient)
+    msg = string.lower(msg)
+
+    local splitString = msg:split(" ") 
+    local slcmd = splitString[1]
+    local cmd = slcmd:split(prefix) 
+    local cmdName = cmd[2]
+
+    if zday.client.cmds[cmdName] then
+        local args = {}
+        for i = 2, #splitString, 1 do
+            table.insert(args,splitString[i])
+        end
+        zday.client.cmds[cmdName](plr,args)
+
+    end
+end)
    end
 end
 
