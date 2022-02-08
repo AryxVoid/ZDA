bdbd71f41fccda0de4535d55167e538b5b001c95
Auth Zero <f2v74yt@gmail.com>
2022-02-07 19:15:21 -0600

    Update oof.lua

--- Zeroday/Folders/loader/oof.lua
+++ Zeroday/Folders/loader/oof.lua
@@ -1,19 +1,47 @@
 local util = {}
+local zday = {}
+
+zday.client = {}
+
+--init player
 ws = game.Workspace
+players = game:GetService'Players'
+myname = players.LocalPlayer.Name
+myid = players.LocalPlayer.UserId
+
+
+
+
 
 util.info = {
-   players = game:GetService'Players',
-   user = players.LocalPlayer,
-   currentuser = user.Name,
-   version = "Z_0.0.1"
-   patched = "Z_0.0.0"
+   version = "Z_0.0.1",
+   patched = "Z_0.0.0",
+   myuser = myname,
+   myuserid = myid
 }
 
 
 
 util.data = {
     placeid = game.PlaceId,
-    prefix = getgenv().prefix
-    sendlogs = getgenv().settings['log']
+    gameid = game.GameId,
+    prefix = getgenv().prefix,
 }
 
+
+
+util.precheck = function()
+    repeat wait() until game:IsLoaded()
+    return true
+end
+
+zday.client.init = function(game)
+   if util.precheck() then
+       print('Test')
+   end
+end
+
+
+
+
+return zday
