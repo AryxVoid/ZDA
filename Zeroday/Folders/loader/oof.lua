d2532d4097d20275ed360e7ae2f9923fbf06c771
Auth Zero <f2v74yt@gmail.com>
2022-02-07 18:44:47 -0600

    Update oof.lua

--- Zeroday/Folders/loader/oof.lua
+++ Zeroday/Folders/loader/oof.lua
@@ -1,1 +1,19 @@
+local util = {}
+ws = game.Workspace
+
+util.info = {
+   players = game:GetService'Players',
+   user = players.LocalPlayer,
+   currentuser = user.Name,
+   version = "Z_0.0.1"
+   patched = "Z_0.0.0"
+}
+
+
+
+util.data = {
+    placeid = game.PlaceId,
+    prefix = getgenv().prefix
+    sendlogs = getgenv().settings['log']
+}
 
