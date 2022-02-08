b52ab60189b70666ff2177c1de2737648f8af305
Auth Zero <f2v74yt@gmail.com>
2022-02-07 19:59:39 -0600

    Update oof.lua

--- Zeroday/Folders/loader/oof.lua
+++ Zeroday/Folders/loader/oof.lua
@@ -40,22 +40,71 @@ util.precheck = function()
     return true
 end
 
+
+util.RemoveSpaces = function(name)
+    return name:gsub('%s+', '') or name
+end
+
+util.finduser = function(name)
+    name = util.RemoveSpaces(name)
+    for i, player in pairs(game:GetService('Players'):GetPlayers()) do
+        if player.Name:lower():match('^'.. name:lower()) then
+            return player
+        end
+    end
+    return nil
+end
+
+
 zday.client.init = function()
    if util.precheck() then
        print('Zero Day Loaded, Using Prefix: ['..util.data.prefix..']')
        wait()
 
 zday.client.cmds.test = function()
-        print("Hello World,"..util.info.adminname.." Working...")
+        print("Hello World, "..util.info.adminname.."  Working...")
 end
+zday.client.cmds.goto = function(origin,args)
+    for i, player in pairs(args) do
+        local playerto = args[1]
 
+        if playerto  then
+            local totpto = util.finduser(playerto)
 
+            if totpto  then
+            HumanoidRootPart.CFrame =  totpto.Character.HumanoidRootPart.CFrame * CFrame.new(0, 4, 0)
+            else
+                if not totpto then
+                    warn("The Player "..player.." Was Not Found!")
+                end
+            end
 
+        end
 
+    end
+end
+zday.client.cmds.view = function(origin,args)
+    for i, player in pairs(args) do
+        local playerto = args[1]
 
+        if playerto  then
+            local totpto = util.finduser(playerto)
 
+            if totpto  then
+                game.Workspace.CurrentCamera.CameraSubject = totpto.Character.Head
+            else
+                if not totpto then
+                    warn("The Player "..player.." Was Not Found!")
+                end
+            end
 
+        end
 
+    end
+end
+zday.client.cmds.unview = function(origin,args)
+    game.Workspace.CurrentCamera.CameraSubject = game.Players.LocalPlayer.Character.Head
+end
 
 zdayuser.Chatted:Connect(function(msg,recipient)
     msg = string.lower(msg)
