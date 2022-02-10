617cd988c4a615fa3a398bbbe01bf9657ba2b5fa
Auth Zero <f2v74yt@gmail.com>
2022-02-09 19:00:54 -0600

    Update oof.lua

--- Zeroday/Folders/loader/oof.lua
+++ Zeroday/Folders/loader/oof.lua
@@ -8,6 +8,56 @@ local zinit = loadstring(game:HttpGet('https://raw.githubusercontent.com/AuthZer
 zday.client = {}
 zday.client.cmds = {}
 
+--notifications
+local notifs = Instance.new("ScreenGui")
+local notify = Instance.new("ImageLabel")
+local TextLabel = Instance.new("TextLabel")
+
+
+
+notifs.Name = "notifs"
+notifs.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+notifs.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+notify.Name = "notify"
+notify.Parent = notifs
+notify.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+notify.BackgroundTransparency = 1.000
+notify.Position = UDim2.new(0.351052076, 0, -0.0468127578, 0)
+notify.Size = UDim2.new(0, 586, 0, 47)
+notify.Image = "rbxassetid://8779590682"
+notify.ImageTransparency = 1.000
+
+TextLabel.Parent = notify
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.BackgroundTransparency = 1.000
+TextLabel.Position = UDim2.new(0.094339624, 0, 0, 0)
+TextLabel.Size = UDim2.new(0, 528, 0, 46)
+TextLabel.Font = Enum.Font.SciFi
+TextLabel.Text = ""
+TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.TextSize = 21.000
+TextLabel.TextTransparency = 1.000
+TextLabel.TextXAlignment = Enum.TextXAlignment.Left
+
+util = {}
+tween = game:GetService'TweenService'
+noti = notify
+notitext = noti.TextLabel
+
+showing = UDim2.new(0.35, 0,-0.001, 0)
+hiding = UDim2.new(0.351, 0,-0.047, 0)
+
+
+info = {
+	tweenInfo = TweenInfo.new(1.5, Enum.EasingStyle.Back, Enum.EasingDirection.Out),
+	notidelay = 0.7,
+	warning = Color3.fromRGB(255, 238, 0),
+	success = Color3.fromRGB(0, 255, 0),
+	failure = Color3.fromRGB(255, 0, 0),
+	none = Color3.fromRGB(255,255,255)
+}
+
 --settings
 
 _G.settings = {
@@ -40,7 +90,8 @@ util.info = {
 }
 
 util.vars = {
-   isflinging = false
+   isflinging = false,
+   notifyshowing = false
 }
 
 util.data = {
@@ -98,100 +149,155 @@ util.finduser = function(name)
     return nil
 end
 
+util.typewrite = function(object,text)
+	for i = 1,#text,1 do
+		object.Text = string.sub(text,1,i)
+		wait()
+	end
+end
+
+util.notify = function(text, kind)
+	tween:Create(noti, info.tweenInfo,{ImageTransparency = 0}):Play()
+	tween:Create(noti, info.tweenInfo,{Position = showing}):Play()
+	wait(info.notidelay)
+	tween:Create(notitext, info.tweenInfo,{TextTransparency = 0}):Play()
+	util.vars.notifyshowing = true
+	if kind == 0 then
+		notitext.TextColor3 = info.success
+	else
+		if kind == 1 then
+			notitext.TextColor3 = info.warning
+			
+			else
+
+			if kind == 2 then
+				notitext.TextColor3 = info.failure
+
+			end
+		end
+	end
+	if util.vars.notifyshowing == true then
+		util.typewrite(notitext, text)
+		wait(1.7)
+		util.typewrite(notitext, '')
+		tween:Create(noti, info.tweenInfo,{Position = hiding}):Play()
+		wait(.2)
+		tween:Create(notitext, info.tweenInfo,{TextTransparency = 1}):Play()
+		wait(.1)
+		tween:Create(noti, info.tweenInfo,{ImageTransparency = 1}):Play()
+		notitext.TextColor3 = info.none
+		util.vars.notifyshowing = false
+		return  util.vars.notifyshowing
+	else
+    return util.vars.notifyshowing
+	end
+	
+end
+
 
 zday.client.init = function()
    if util.precheck() then
-       print('Zero Day Loaded')
+      util.notify('Zero Day Loaded',0)
        wait()
 
-zday.client.cmds.test = function()
-        print("Hello World, "..util.info.adminname.."  Working...")
-end
-  
-zday.client.cmds.spf = function(origin,args)
-   local newprefixinfo = args[1]
-   _G.settings.prefix = newprefixinfo
-   print("Saved new prefix to, ".. _G.settings.file.." ")
-   util.saveSettings()
-end
-  
-zday.client.cmds.pf = function(origin,args)
-   print("Current prefix:  ".. _G.settings.prefix.." ")
-end
-  
-zday.client.cmds.to = function(origin,args)
-    for i, player in pairs(args) do
-        local playerto = args[1]
+    zday.client.cmds.test = function()
+        util.notify("Hello World, "..util.info.adminname.."  Working...", 0)
+    end
 
-        if playerto  then
-            local totpto = util.finduser(playerto)
+    zday.client.cmds.spf = function(origin,args)
+        local newprefixinfo = args[1]
+        _G.settings.prefix = newprefixinfo
+        util.notify("Prefix Switched To:  ".. _G.settings.prefix.." ",1)
+        util.saveSettings()
+    end
 
-            if totpto  then
-            HumanoidRootPart.CFrame =  totpto.Character.HumanoidRootPart.CFrame * CFrame.new(0, 4, 0)
-            else
-                if not totpto then
-                    warn("The Player "..player.." Was Not Found!")
+    zday.client.cmds.pf = function(origin,args)
+        util.notify("Current prefix:  ".. _G.settings.prefix.." ",0)
+    end
+
+    zday.client.cmds.to = function(origin,args)
+        for i, player in pairs(args) do
+            local playerto = args[1]
+
+            if playerto  then
+                local totpto = util.finduser(playerto)
+
+                if totpto  then
+                    HumanoidRootPart.CFrame =  totpto.Character.HumanoidRootPart.CFrame * CFrame.new(0, 4, 0)
+                    util.notify('Teleported to: '..totpto, 1)
+                else
+                    if not totpto then
+                        util.notify("The Player "..player.." Was Not Found!", 2)
+                    end
                 end
+
             end
 
         end
-
     end
-end
-zday.client.cmds.view = function(origin,args)
-    for i, player in pairs(args) do
-        local playerto = args[1]
+    zday.client.cmds.view = function(origin,args)
+        for i, player in pairs(args) do
+            local playerto = args[1]
 
-        if playerto  then
-            local totpto = util.finduser(playerto)
+            if playerto  then
+                local totpto = util.finduser(playerto)
 
-            if totpto  then
-                game.Workspace.CurrentCamera.CameraSubject = totpto.Character.Head
-            else
-                if not totpto then
-                    warn("The Player "..player.." Was Not Found!")
+                if totpto  then
+                    game.Workspace.CurrentCamera.CameraSubject = totpto.Character.Head
+                    util.notify('Viewing: '..totpto, 1)
+                else
+                    if not totpto then
+                        util.notify("The Player "..player.." Was Not Found!", 2)
+                    end
                 end
+
             end
 
         end
-
     end
-end
-zday.client.cmds.unview = function(origin,args)
-    game.Workspace.CurrentCamera.CameraSubject = game.Players.LocalPlayer.Character.Head
-end
-  
-zday.client.cmds.edit = function(origin, args)
-     local setting = args[1]
-     local state = args[2]
-     _G.settings[setting] = state
-     warn('Edited setting: '..setting..' Changed value to : '..state)
-     util.saveSettings()
- end
+    zday.client.cmds.unview = function(origin,args)
+        util.notify('Unviewed', 1)
+        game.Workspace.CurrentCamera.CameraSubject = game.Players.LocalPlayer.Character.Head
+    end
 
-zdayuser.Chatted:Connect(function(msg,recipient)
-    msg = string.lower(msg)
-
-    local splitString = msg:split(" ") 
-    local slcmd = splitString[1]
-    local cmd = slcmd:split(_G.settings.prefix) 
-    local cmdName = cmd[2]
-
-    if zday.client.cmds[cmdName] then
-        local args = {}
-        for i = 2, #splitString, 1 do
-            table.insert(args,splitString[i])
+    zday.client.cmds.edit = function(origin, args)
+        local setting = args[1]
+        local state = args[2]
+        if _G.settings[setting] then
+            _G.settings[setting] = state
+            util.notify('Edited setting: '..setting..' Changed value to : '..state,1)
+            util.saveSettings()
+        else
+            util.notify('Setting: '..setting..' Does not exist. ',2)
+             return setting, state
         end
-        zday.client.cmds[cmdName](plr,args)
-
     end
-end)
+    
+    zday.client.cmds.settings = function()
+        util.notify('Copied To Clipboard', 1)
+        setclipboard(_G.settings)
+    end
+
+    zdayuser.Chatted:Connect(function(msg,recipient)
+        msg = string.lower(msg)
+
+        local splitString = msg:split(" ") 
+        local slcmd = splitString[1]
+        local cmd = slcmd:split(_G.settings.prefix) 
+        local cmdName = cmd[2]
+
+        if zday.client.cmds[cmdName] then
+            local args = {}
+            for i = 2, #splitString, 1 do
+                table.insert(args,splitString[i])
+            end
+            zday.client.cmds[cmdName](zdayuser,args)
+
+        end
+    end)
    end
 end
 
-zday.client.reload = function()
-  local version = {}
-end
 
 
 --Zera was here @2022
